b6f6d57f063794e4e35cd7bd6f6d0f890010085d
9uTae <ddr04099@gmail.com>
2022-05-29 17:34:17 +0900

    DiaryItem, DiaryList 정리

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import DiaryItem from "./DiaryItem";
 import MyButton from "./MyButton";
